d2febc13a23265cc7c2f9d4d109096acb7a05b10
Sean Harger <seanharger93@gmail.com>
2020-03-15 11:38:26 -0700

    Add basic main function that parses flags.

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
+// enhance_faa_cifp augments FAA CIFP (coded instrument flight procedure) data
+// for flight simulators.
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
+var outFile = flag.String("output", "FAACIFP_augmented", "path of the file to output augmented procedures")
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintln(flag.CommandLine.Output(), "Usage: enhance_faa_cifp [options...] <cifp_file>")
+		flag.PrintDefaults()
+	}
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	flag.Parse()
+	if len(flag.Args()) < 1 {
+		log.Fatalf("Must specify a CIFP file.")
+	}
+	cifpFile := flag.Args()[0]
+	log.Printf("CIFP file: %q", cifpFile)
+	log.Printf("CIFP output file: %q", *outFile)
 }
